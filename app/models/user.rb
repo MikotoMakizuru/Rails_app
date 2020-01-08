88f1f3264cc2b7ad7ea47dd399d2dd87a859772f
Makizushi3510max <mikoto.makizuru3510@icloud.com>
2020-01-08 14:30:37 +0900

    bug-fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,13 +51,15 @@ class User < ApplicationRecord
   end
 
   private
-    #メールアドレスを全て小文字にする
+
+    # メールアドレスをすべて小文字にする
     def downcase_email
       self.email = email.downcase
     end
 
-    def create_actvation_digest
-      self.activation_token = User.new_token
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
 end
